f3eae47510e39eabfc08b92f2d963afcec9e3abd
Swapnil N <swapnil.napuri@gmail.com>
2021-03-15 00:47:19 -0400

    implemented Pawn movement

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -4,32 +4,65 @@ import chess.Board;
 
 public class Pawn extends Piece{
 	
-	int initial;
-
 	public Pawn(char color, char col, int row) {
 		super(color, col, row);
-		initial = 0;
 	}
 
 	@Override
-	public boolean move(Board board, char newCol, int newRow) {
+	public boolean canMove(Board board, char newCol, int newRow) {
 		if (newRow < 1 || newRow > board.getDimension()-1)
 			return false;
 		if (newCol < 'a' || newCol > 'h')
-			return false;
-		/*
-		if (Math.abs(newCol - getCol()) > 1)
-			return false;
-		
+			return false;	
+
 		if (getColor() == 'w'){
-			if (newRow <= getRow())
-				return false;
+			if (newCol == getCol()){
+				if (board.getPiece(newCol, getRow()+1) != null)
+					return false;
+				if (getRow() == 2 && newRow == 4){
+					setCol(newCol);
+					setRow(newRow);
+					return true;
+				}
+				if (getRow()+1 == newRow){
+					setCol(newCol);
+					setRow(newRow);
+					return true;
+				}
+			}
+			else if(getRow()+1 == newRow && (getCol()+1 == newCol || getCol()-1 == newCol)){
+				if (board.getPiece(newCol, newRow) != null && board.getPiece(newCol, newRow).getColor() == 'b'){
+					setCol(newCol);
+					setRow(newRow);
+					return true;
+				}
+			}
 		}
-		
-		if (getColor() == 'w' && getRow() == )
-		
-		*/
-		initial = 1;
+
+		if (getColor() == 'b'){
+			if (newCol == getCol()){
+				if (board.getPiece(newCol, getRow()-1) != null)
+					return false;
+				if (getRow() == 7 && newRow == 5){
+					setCol(newCol);
+					setRow(newRow);
+					return true;
+				}
+				if (getRow()-1 == newRow){
+					setCol(newCol);
+					setRow(newRow);
+					return true;
+				}
+			}
+			else if(getRow()+1 == newRow && (getCol()+1 == newCol || getCol()-1 == newCol)){
+				if (board.getPiece(newCol, newRow) != null && board.getPiece(newCol, newRow).getColor() == 'w'){
+					setCol(newCol);
+					setRow(newRow);
+					return true;
+				}
+			}
+		}
+
 		return false;
 	}
 
